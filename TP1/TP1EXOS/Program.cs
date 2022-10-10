cc9dc39aa4bcfccd1b250d78fb64335508be01c0
Enzo Neault <aza.woodyy@gmail.com>
2022-10-10 14:23:45 +0200

    :tada: Config and start Poo course

---
+++ TP1/TP1EXOS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Pays {
+    public string Name { get; set; }
+
+    public Pays(string name) {
+        this.Name = name;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+        Pays p1 = new Pays("France");
+        Pays p2 = new Pays("USA");
+        p2.Name = "Espagne";
+        Console.WriteLine(p1.Name + " > " + p2.Name);
+    }
+}
